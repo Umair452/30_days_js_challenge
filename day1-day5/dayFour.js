4914696748540d25abb6e8904a661416cf68cd38
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-07-19 07:48:16 +0000

    loops

---
+++ day1-day5/dayFour.js
@@ -0,0 +1,47 @@
+for (let index = 1; index <= 10; index++) {
+    // console.log(index);
+    
+}
+
+for (let index = 1; index <= 10; index++) {
+    // console.log(`5    *    ${index}  = ${5 * index}`);
+    
+}
+
+let index = 1
+let sum = 0
+
+while (index <= 10) {
+
+    sum = sum + index
+    index++
+
+
+}
+
+
+let reverseIndex = 10
+
+while (reverseIndex >= 1){
+    // console.log(reverseIndex);
+    reverseIndex--
+}
+
+// console.log(`Sum of numbers from 1 to 10 is: ${sum}`);
+
+let doIndex = 5
+let ficIndex = 1
+let ficResult = 1
+do {
+   ficResult = ficIndex * ficResult
+   ficIndex++
+} while (ficIndex<= doIndex);
+// console.log(`Fictorial of ${doIndex} is: ${ficResult}`);
+
+let staric = ' * '
+for (let index = 0; index <= 5; index++) {
+    console.log(`${staric}`);
+    staric += staric
+    index++ 
+    
+}
