774c7a20c1600da6c0b089e2a7079ad4faca743f
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-07-27 07:18:42 +0000

    day six

--- day5-10/six.js
+++ day5-10/six.js
@@ -31,8 +31,63 @@ let addToCart = []
 
 
 
+
+
+
 let newArray = myArray.map(function(element){
     return element * 2;
 })
 // console.log(`Old array is ${myArray}`);
 // console.log(`New array is ${newArray}`);
+
+// discount using map function
+
+let customerCart = [300, 2000, 1500];
+let discountedCart = [];
+let prevTotal = 0;
+let newTotal = 0;
+
+customerCart.forEach(element => {
+    prevTotal = element + prevTotal
+     
+});
+
+const discount = true;
+
+if (discount == true) {
+    discountedCart = customerCart.map(function(element){
+        let discount = element * 0.2
+        return element = element - discount
+    })
+} else {
+    return customerCart
+}
+
+discountedCart.forEach(element => {
+    newTotal += element
+})
+
+// console.log(`Cart total before discount was: ${prevTotal}`);
+// console.log(`Cart total after discount is: ${newTotal}`);
+
+
+let filterArray = [1,2,3,4,5,6,7,8,9,10]
+
+// console.log(filterArray.filter((num) => num % 2 == 0));
+
+filterArray.filter((e, index) => {
+// console.log(`Index of elment is: ${index} and the element is:${e}`);    
+});
+
+console.log(filterArray.reduce((acc, num) => {
+    
+    return acc +=  num
+}));
+
+//use reduce and filter methods for discount and other things
+
+console.log(`Your new bill after discount is ${
+    customerCart.reduce((acc, item) => {
+        return acc += item
+    })
+}`);
