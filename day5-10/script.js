5bc85790d85ede77ef2b73118221ffc1ac374de0
Umair452 <arshadu452@gmail.com>
2024-08-07 18:35:53 +0500

    event handling is awesome

--- day5-10/script.js
+++ day5-10/script.js
@@ -45,3 +45,69 @@ function borderChanger(){
     }
     
 }
+
+function visibiltyChanger () {
+    const image = document.querySelector('#image')
+    if (image.style.opacity == 0) {
+        image.style.opacity = 1
+    } else {
+        image.style.opacity = 0
+    }
+}
+
+function pressedKey() {
+    const input = document.querySelector('#input').value
+    console.log(`You pressed ${input} key`);
+    
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+document.querySelector('#form').addEventListener('submit', formHandler);
+
+function formHandler(event) {
+    event.preventDefault(); // Prevent the form from submitting and reloading the page
+    
+    const formData = {
+        name: document.querySelector('#name').value,
+        email: document.querySelector('#email').value,
+        phone: document.querySelector('#phone').value,
+        message: document.querySelector('#message').value
+    };
+
+
+
+    console.log(formData); // Log the form data to the console
+}
+
+
+const element = document.querySelector('#dropdown');
+const hero = document.querySelector('#heroName');
+hero.textContent = element.firstChild.value
+
+element.addEventListener('change' , (event)=>{
+    
+    hero.textContent = event.target.value;
+})
+
+const list = document.querySelector('.list')
+list.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        console.log(`You clicked ${event.target.innerText} `);
+        
+    }
+})
+
+
+  
+
+
+
+})
+
+function elementCreator() {
+    const list = document.querySelector('.list')
+   const newElement =  document.createElement('li')
+   newElement.textContent = "I am added by button"
+   list.appendChild(newElement)
+}
